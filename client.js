038580bf833ae0476072383111939dfd1b23a7c7
Arrielle <arrielle.kooiman@gmail.com>
2017-02-08 10:04:00 -0600

    Working through concatenation

--- client.js
+++ client.js
@@ -15,17 +15,32 @@
 
 // After I got my code to work, I realized I was never asked to turn the array and chosen number a variable so rewrote it;
 
-myFunction(["DON'T", "WANT", "THESE", "WORDS", "I", "like", "to", "eat", "cake."], 4);
+// myFunction(["DON'T", "WANT", "THESE", "WORDS", "I", "like", "to", "eat", "cake."], 4);
 
-function myFunction(array, num){
-  for (var i = 0; i < array.length; i++) {
-    if (i >= num){
-      // console.log(array[i]);
-        var arrayString = (array.substring(num, array.length));
-        console.log(arrayString);
-    }
-  }
+// function myFunction(array, num){
+//   for (var i = 0; i < array.length; i++) {
+//     if (i >= num){
+//       // console.log(array[i]);
+//         var arrayString = (array.substring(num + 1, array.length));
+//         console.log(arrayString);
+//     }
+//   }
+// }
+
+//Working through with the group - 
+
+var stringResult = ''; //an empty string
+var wordArray = ["DON'T", "WANT", "THESE", "WORDS", "I", "like", "to", "eat", "cake."];
+
+
+for (var i = 0; i < wordArray.length; i++) {
+  if (i >= 4) {
+  // console.log(stringResult);
+  stringResult = stringResult + " " + wordArray[i]; //adding word to string result but there is a space in front now.
+} else {
+  i++;
+}
 }
 
-
+console.log(stringResult);
 
